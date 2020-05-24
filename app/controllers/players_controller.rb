126f2ac997ed07f974c8076b48d4d6965717ac41
willy chang <jie255057@gmail.com>
2020-05-24 17:51:42 +0800

    CRUD

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,43 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
   end
 
   def new
     @player = Player.new
+  end
+
+  def create
+    @player = Player.new(player_params)
+    if @player.save
+      redirect_to players_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @player = Player.find(params[:id])
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    if @player.update(player_params)
+      redirect_to players_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @player = Player.find(params[:id])
+    if @player.destroy
+      render players_path
+    end
+  end
+
+  private
+  def player_params
+    params.require(:player).permit(:name, :age, :height, :weight, :hometown)
   end
 end
