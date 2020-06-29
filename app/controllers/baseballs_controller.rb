c5d820c63f4eaec4c0df9f04163b31960a33b69e
willy chang <jie255057@gmail.com>
2020-06-29 23:13:11 +0800

    add team routes

--- app/controllers/baseballs_controller.rb
+++ app/controllers/baseballs_controller.rb
@@ -2,6 +2,9 @@ class BaseballsController < ApplicationController
   def index
   end
 
+  def team
+  end
+
   def new
     @player = Player.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :baseballs
   resources :players
+  get :team, to: 'baseballs#team'
   root 'baseballs#index'
 end
